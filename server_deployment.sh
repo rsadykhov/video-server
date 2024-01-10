cf6fc1160486f8685aefd5cd1e41ce8a5a13ba64
Rem Sadykhov <94738900+rsadykhov@users.noreply.github.com>
2024-01-10 19:36:37 +0000

    Extra

--- server_deployment.sh
+++ server_deployment.sh
@@ -1,6 +1,8 @@
 cd video_server
 python3 manage.py makemigrations users
 python3 manage.py migrate users
+python3 manage.py makemigrations videos
+python3 manage.py migrate videos
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
